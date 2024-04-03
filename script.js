abd9a6ba0c44f2537e417da8b61881954ec7e416
Clayton Cirqueira <cirqueira.info@gmail.com>
2024-04-03 11:27:35 -0300

    capturing elements for question and answer

--- script.js
+++ script.js
@@ -4,73 +4,29 @@ const questions = [
         // question nº1
         question:"Which is larget animal in the world?",
         answers:[
-            {text: "Shark", Correct:false},
-            {text: "Blue whale", Correct:true},
-            {text: "Elephant", Correct:false},
-            {text: "Giraffe", Correct:false},
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
 
         ]
     },
     {
         // question nº2
-        question:"Which is larget animal in the world?",
+        question:"_Which is larget animal in the world?",
         answers:[
-            {text: "Shark", Correct:false},
-            {text: "Blue whale", Correct:true},
-            {text: "Elephant", Correct:false},
-            {text: "Giraffe", Correct:false},
+            {text: "_Shark", correct: false},
+            {text: "_Blue whale", correct: true},
+            {text: "_Elephant", correct: false},
+            {text: "_Giraffe", correct: false},
 
         ]
-    },
-    {
-        // question nº3
-        question:"Which is larget animal in the world?",
-        answers:[
-            {text: "Shark", Correct:false},
-            {text: "Blue whale", Correct:true},
-            {text: "Elephant", Correct:false},
-            {text: "Giraffe", Correct:false},
-
-        ]
-    },
-    {
-        // question nº4
-        question:"Which is larget animal in the world?",
-        answers:[
-            {text: "Shark", Correct:false},
-            {text: "Blue whale", Correct:true},
-            {text: "Elephant", Correct:false},
-            {text: "Giraffe", Correct:false},
-
-        ]
-    },
-    {
-        // question nº5
-        question:"Which is larget animal in the world?",
-        answers:[
-            {text: "Shark", Correct:false},
-            {text: "Blue whale", Correct:true},
-            {text: "Elephant", Correct:false},
-            {text: "Giraffe", Correct:false},
-
-        ]
-    },
-     {
-        // question nº6
-        question:"Which is larget animal in the world?",
-        answers:[
-            {text: "Shark", Correct:false},
-            {text: "Blue whale", Correct:true},
-            {text: "Elephant", Correct:false},
-            {text: "Giraffe", Correct:false},
-
-        ]
-     }   
+    }            
 ];
 
 //questions
 const questionElement = document.getElementById("question");
-const answerButton = document.getElementById("answer-buttons");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
 //score
@@ -80,13 +36,46 @@ let score = 0;
 // start-up test
 function startQuiz(){
     currentQuestionIndex = 0;
+    score = 0;
     nextButton.innerHTML = "Next";
     showQuestion();
 }
 
 //managing questions
 function showQuestion(){
+    resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". "+currentQuestion.question;
+    questionElement.innerHTML = questionNo + ". "+ currentQuestion.
+    question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click",selectAnswer);
+    });
 }
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+}
+
+startQuiz();
