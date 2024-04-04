a9d13a48a797bb9094569687ec20e95a4cb0e808
Clayton Cirqueira <cirqueira.info@gmail.com>
2024-04-03 22:49:44 -0300

    new questions and functionality

--- script.js
+++ script.js
@@ -2,26 +2,49 @@
 const questions = [
     {
         // question nº1
-        question:"Which is larget animal in the world?",
+        question:"sobre lei 8.112/90: Rezende era um servidor estável, mas foi inabilitado no estágio probatório após ser aprovado em concurso para outro cargo no mesmo órgão. ",
         answers:[
-            {text: "Shark", correct: false},
-            {text: "Blue whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Revisão", correct: false},
+            {text: "Reinclusão", correct: false},
+            {text: "Recondução", correct: true},
+            {text: "Reintegração", correct: false},
 
         ]
     },
     {
         // question nº2
-        question:"_Which is larget animal in the world?",
+        question:"sobre lei 8.112/90: Qual é o procedimento aplicável quando um servidor público estável é inabilitado no estágio probatório após ser aprovado em concurso para outro cargo no mesmo órgão?",
         answers:[
-            {text: "_Shark", correct: false},
-            {text: "_Blue whale", correct: true},
-            {text: "_Elephant", correct: false},
-            {text: "_Giraffe", correct: false},
+            {text: "Reintegração", correct: false},
+            {text: "Recondução", correct: true},
+            {text: "Revisão", correct: false},
+            {text: "Reinclusão", correct: false},
 
         ]
-    }            
+    },
+    {
+        // question nº3
+        question:"Qual é o papel do emissor na comunicação?",
+        answers:[
+            {text: "Privilegia a melodia e sonoridade das palavras.", correct: false},
+            {text: "Utiliza uma linguagem elaborada e cuidada.", correct: false},
+            {text: "Utiliza uma linguagem denotativa.", correct: false},
+            {text: " Representa aquele que envia a mensagem.", correct: true},
+
+        ]
+    },
+    {
+        // question nº4
+        question:"Quem é o receptor na comunicação?",
+        answers:[
+            {text: "Só existe comunicação quando a pessoa que recebe a mensagem entende o seu significado.", correct: false},
+            {text: "O conceito de sociedade de massa remonta aos anos de escravidão, quando pessoas das diversas tribos africanas eram vistas como um grupo homogêneo.", correct: false},
+            {text: "É representado por aquele que recebe a mensagem.", correct: true},
+            {text: "A constatação da existência de uma sociedade de massa invalidou os fundamentos da teoria hipodérmica.", correct: false},
+
+        ]
+    },
+                 
 ];
 
 //questions
@@ -73,9 +96,11 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add("correct");
+        score++;
     }else{
         selectedBtn.classList.add("incorrect");
     }
+
     // when clicking on an option, it should show the correct one and disable the others
     Array.from(answerButtons.children).forEach(button =>{
         if(button.dataset.correct === "true"){
@@ -85,5 +110,29 @@ function selectAnswer(e){
     });
     nextButton.style.display = "block";
 }
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", ()=>{
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();        
+    }
+});
 
 startQuiz();
